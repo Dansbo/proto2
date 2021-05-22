27de846dc90d83622642595f721416cc5d62047a
Jimmy Dansbo <jimmy@dansbo.dk>
2021-05-22 23:49:50 +0200

    Added includes to main file

--- proto2.asm
+++ proto2.asm
@@ -1,9 +1,15 @@
 !cpu w65c02
-;******************************* Boiler plate *********************************
-*=$801
-	!word	$080C			; Pointer to next BASIC line
-	!word	$0001			; Line number $0001 = 1
-	!byte	$9E			; SYS BASIC token
-	!pet	" $810",0		; Address where ASM starts
-	!word	$0000			; EOF BASIC program
-*=$810
+
+!src "cx16.inc"
++SYS_LINE
+
+!src "vera0.9.inc"
+!src "globals.inc"
+!src "functions.inc"
+!src "TextUI.inc"
+
+;******************************************************************************
+; Main program starts here.
+;******************************************************************************
+main:
+	rts
