abaa07f63e503b97f8379e4ac74f842692de69c1
Jimmy Dansbo <jimmy@dansbo.dk>
2021-07-22 10:45:38 +0200

    Hacked main program file to enable playing more than one level.

--- proto2.asm
+++ proto2.asm
@@ -19,6 +19,7 @@ main:
 	jsr	load_chars	; Load special chars to draw gates into font memory
 	jsr	Init_VERA
 	jsr	Print_Title
+@next_level:
 	jsr	load_level
 
 @loop:
@@ -29,7 +30,31 @@ main:
 	jsr	check_inputs
 	jsr	check_gates
 	bcc	@loop
-
+	; Tell that level is solved
+	lda	#GAME_GRID_TOP_LEFT_X+10
+	ldy	#GAME_GRID_TOP_LEFT_Y-2
+	jsr	VTUI_gotoxy
+	lda	#1
+	jsr	VTUI_set_stride
+	bra	@end_solv
+@solv:	!pet	"solved - press space"
+@end_solv:
+	ldy	#@end_solv-@solv
+	lda	#<@solv
+	sta	r0l
+	lda	#>@solv
+	sta	r0h
+	ldx	#(BLACK<<4)|WHITE
+	jsr	VTUI_print_str
+	; Wait for keypress
+-	jsr	GETIN
+	cmp	#0
+	beq	-
+	; Can not jump directly to function that gets next level number as it
+	; is a cheap local variable. Jumping to line 121 in TextUI.inc,
+	; just below @Choice label
+	jsr	Print_Title+$33C
+	bra	@next_level
 @end:
 	rts
 
