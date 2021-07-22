29f32bb9e874edf4e79f9b4d9e8956357999783e
Jimmy Dansbo <jimmy@dansbo.dk>
2021-07-22 11:27:39 +0200

    Added explaining comments to my hack of the gameplay.

--- proto2.asm
+++ proto2.asm
@@ -30,22 +30,23 @@ main:
 	jsr	check_inputs
 	jsr	check_gates
 	bcc	@loop
+
 	; Tell that level is solved
 	lda	#GAME_GRID_TOP_LEFT_X+10
 	ldy	#GAME_GRID_TOP_LEFT_Y-2
-	jsr	VTUI_gotoxy
+	jsr	VTUI_gotoxy		; Place cursor appropriately on screen
 	lda	#1
-	jsr	VTUI_set_stride
-	bra	@end_solv
+	jsr	VTUI_set_stride		; Ensure VERA stride is set correctly
+	bra	@end_solv		; Jump over string to print
 @solv:	!pet	"solved - press space"
 @end_solv:
-	ldy	#@end_solv-@solv
-	lda	#<@solv
+	ldy	#@end_solv-@solv	; Length of string
+	lda	#<@solv			; low address of string
 	sta	r0l
-	lda	#>@solv
+	lda	#>@solv			; high address of string
 	sta	r0h
-	ldx	#(BLACK<<4)|WHITE
-	jsr	VTUI_print_str
+	ldx	#(BLACK<<4)|WHITE	; White text on black background
+	jsr	VTUI_print_str		; Print string to let user know level is solved
 	; Wait for keypress
 -	jsr	GETIN
 	cmp	#0
@@ -53,7 +54,7 @@ main:
 	; Can not jump directly to function that gets next level number as it
 	; is a cheap local variable. Jumping to line 121 in TextUI.inc,
 	; just below @Choice label
-	jsr	Print_Title+$33C
+	jsr	Print_Title+$33C	; Ask user for next level to play
 	bra	@next_level
 @end:
 	rts
